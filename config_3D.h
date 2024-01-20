81e5ed5f3c063a623d57a397b939280ff67343dd
TobiasBodewig <tbodewig@web.de>
2024-01-20 11:35:12 +0100

    Add sample config file.

---
+++ config_3D.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_3D_H
+#define CONFIG_3D_H
+
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "password"
+#define SERVER_IP "0.0.0.0"
+#define AUTHENTICATION_TOKEN "token"
+
+#define MACHINE_NAME "name" //defines the machine group for chekcing the permission
+#define MACHINE_ID  "id" //used for the logfile in the backend 
+
+#endif /* CONFIG_3D_H */
